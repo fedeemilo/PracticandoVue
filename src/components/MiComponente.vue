5dfdc8550ae27cca80fdc0c5b9638f76b0054edd
fedeemilo <fedeemilo91@gmail.com>
2020-01-10 00:46:08 -0300

    Initial Commit

---
+++ src/components/MiComponente.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="MiComponente">
+		<h1>Ésta es mi primera app contruida con Vue.js</h1>
+        <h3>Tiene una curva de aprendizaje muy baja y he aprendido mucho en sólo 2 días!</h3>
+	</div>
+</template>
+
+<script>
+// Con éste comando exporto el componente dentro de la app
+export default {
+	// Defino las propiedades del componente
+	name: 'MiComponente',
+	data() {
+		return {};
+	}
+};
+</script>
+
+<style></style>
